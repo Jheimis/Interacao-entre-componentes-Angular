9c6a01466297254903e873949f91e95cff8eff51
Jheimis Fernandes Marques <jheimis96@gmail.com>
2020-10-05 16:38:32 -0300

    Criação e configuração dos componentes Pai e Filho

---
+++ src/app/filho/filho.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filho',
+  templateUrl: './filho.component.html',
+  styleUrls: ['./filho.component.css']
+})
+export class FilhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
