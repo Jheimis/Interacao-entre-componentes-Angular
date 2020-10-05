cb035797d66433d7ab22d79179113d555d869547
Jheimis Fernandes Marques <jheimis96@gmail.com>
2020-10-05 17:28:21 -0300

    Interceptando e mudando propriedades input usando um setter

--- src/app/filho/filho.component.ts
+++ src/app/filho/filho.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-filho',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filho.component.css']
 })
 export class FilhoComponent implements OnInit {
+
+  @Input() nomeFilho:string;
 
   constructor() { }
 
--- src/app/pai/pai.component.ts
+++ src/app/pai/pai.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaiComponent implements OnInit {
 
+  nomePai = 'Eduardo';
+
   constructor() { }
 
   ngOnInit(): void {
