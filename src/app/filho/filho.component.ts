7dfe41ed3a4dd85f706454284b7703c0a82e760d
Jheimis Fernandes Marques <jheimis96@gmail.com>
2020-10-06 09:16:43 -0300

    Interagindo com o componente filho através de variáveis locais

--- src/app/filho/filho.component.ts
+++ src/app/filho/filho.component.ts
@@ -7,16 +7,7 @@ import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@
 })
 export class FilhoComponent implements OnInit {
 
-
-  @Input() pais : string; /*@Input pais recebe o nome dos países; */
-
-  @Output() votado = new EventEmitter<string>();  /*EventEmitter é criado e associado à variável decorada votado. Já o decorator @Output 
-  permite que os dados dessa variável sejam passados para o componente pai.*/
-
-  vota(pais: string){  /* Através do método vota o nome do país é passado para componente pai através do this.votado.emit(pais).*/ 
-
-    this.votado.emit(pais);
-  }
+  nome = 'Clark Kent';
 
   constructor() { }
 
